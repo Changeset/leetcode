8f4c1b0d6ba1d06ccde79e22562af8307914fb57
Changeset <695700827@qq.com>
2018-03-15 21:24:53 +0800

    update OddEven LinkedList problem

--- src/linkedList/OddEvenLinkedList.java
+++ src/linkedList/OddEvenLinkedList.java
@@ -7,21 +7,25 @@ package linkedList;
  * @ ModifiedBy:
  */
 public class OddEvenLinkedList {
-    public ListNode oddEvenList(ListNode head) {
-        ListNode oddNode = head;
-        ListNode evenNode = head.next;
-        if (evenNode == null) return oddNode;
-        int n = 0;
-        if (oddNode != null ) {
-            n++;
-            oddNode = oddNode.next;
+    public static ListNode oddEvenList(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode odd = head, ehead = head.next, even = ehead;
+        while (even != null && even.next != null) {
+            odd.next = even.next;
+            odd = odd.next;
+            even.next = odd.next;
+            even = even.next;
         }
-        return null;
+        odd.next = ehead;
+        return head;
     }
 
-    public void exch(ListNode node1, ListNode node2) {
-        int temp = node1.val;
-        node1.val = node2.val;
-        node2.val = temp;
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        System.out.println(OddEvenLinkedList.oddEvenList(head));
+        System.out.println();
     }
 }
