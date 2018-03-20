6311a01b56d58485f7e7a6ac65103ad0205f0b28
changeset <695700827@qq.com>
2018-03-20 23:57:53 +0800

    update coin change in dynamic programming.

--- src/dynamicprogramming/CoinChange.java
+++ src/dynamicprogramming/CoinChange.java
@@ -9,40 +9,40 @@ import java.util.Arrays;
  * @ ModifiedBy:
  */
 public class CoinChange {
-    int count = 0;
+//    public int coinChange(int[] coins, int amount) {
+//        if (amount < 1) return 0;
+//        return coinChange(coins, amount, new int[amount]);
+//    }
+//
+//    private int coinChange(int[] coins, int rem, int[] count) {
+//        if (rem < 0) return -1;
+//        if (rem == 0) return 0;
+//        if (count[rem - 1] != 0) return count[rem - 1];
+//        int min = Integer.MAX_VALUE;
+//        for (int coin : coins) {
+//            int res = coinChange(coins, rem - coin, count);
+//            if (res >= 0 && res < min) min = 1 + res;
+//        }
+//        count[rem - 1] = (min == Integer.MAX_VALUE) ? -1 : min;
+//        return count[rem - 1];
+//    }
     public int coinChange(int[] coins, int amount) {
-        if (coins.length == 0) return -1;
-        Arrays.sort(coins);
-        boolean flag = isMakeUp(coins, coins.length - 1, amount, amount);
-        if (!flag) return -1;
-        else return count;
-    }
-
-    public boolean isMakeUp(int[] coins, int end, int amount, int n) {
-        if (amount == n) count = 0;
-        if (end < 0) return false;
-        int endCount = amount / coins[end];
-        if (amount % coins[end] == 0) {
-            count += endCount;
-            return true;
-        }
-        else {
-            for (int i = 0; i < endCount; i++) {
-                if (isMakeUp(coins, end-1, leaveChange(coins[end], amount) + coins[end] * i, n)) {
-                    count += endCount - i;
-                    return true;
+        int max = amount + 1;
+        int[] dp = new int[max];
+        Arrays.fill(dp, max);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            for (int j = 0; j < coins.length; j++) {
+                if (coins[j] <= i) {
+                    dp[i] = Math.min(dp[i], dp[i-coins[j]] + 1);
                 }
             }
-            return isMakeUp(coins, end-1, amount, n);
         }
+        return dp[amount] > amount ? -1 : dp[amount];
     }
-    public int leaveChange(int coins, int amount) {
-        return amount % coins;
-    }
-
     public static void main(String[] args) {
-        int[] nums = {186, 419, 83, 408};
+        int[] nums = {346, 29, 395, 188, 155, 109};
         CoinChange c = new CoinChange();
-        System.out.println(c.coinChange(nums, 6249));
+        System.out.println(c.coinChange(nums, 9401));
     }
 }
