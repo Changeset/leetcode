03fda3d09f5403c03e8246af0d8d39cc9420bf34
Changeset <695700827@qq.com>
2018-04-02 16:38:10 +0800

    update dynamic programming problems

--- src/dynamicprogramming/MaximumProductSubarray.java
+++ src/dynamicprogramming/MaximumProductSubarray.java
@@ -7,5 +7,26 @@ package dynamicprogramming;
  * @ ModifiedBy:
  */
 public class MaximumProductSubarray {
+    public int maxProduct(int[] nums) {
+        if (nums.length == 0) return 0;
+        int maxPro = Integer.MIN_VALUE, imax = nums[0], imin = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] < 0) {
+                int temp = imax;
+                imax = imin;
+                imin = temp;
+            }
+            imax = Math.max(nums[i], imax * nums[i]);
+            imin = Math.min(nums[i], imin * nums[i]);
 
+            maxPro = Math.max(imax, maxPro);
+        }
+        return maxPro;
+    }
+
+    public static void main(String[] args) {
+        MaximumProductSubarray m = new MaximumProductSubarray();
+        int[] nums = {2,3,-2,4,-2};
+        System.out.println(m.maxProduct(nums));
+    }
 }
