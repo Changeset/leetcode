921c0432d06300a415a590206139572e7013ecd1
Changeset <695700827@qq.com>
2018-04-27 15:24:26 +0800

    update Link List problems

---
+++ src/linkedList/RandomListNode.java
@@ -0,0 +1,13 @@
+package linkedList;
+
+/**
+ * @ Author: Xuelong Liao
+ * @ Description:
+ * @ Date: created in 11:27 2018/4/27
+ * @ ModifiedBy:
+ */
+public class RandomListNode {
+    int label;
+    RandomListNode next, random;
+    RandomListNode(int x) { this.label = x; }
+}
