a42b4cf1fafdecb562aa444a79da68094dcb5c64
Changeset <695700827@qq.com>
2018-05-02 14:39:46 +0800

    update hashtable problems

---
+++ src/hashtable/Point.java
@@ -0,0 +1,14 @@
+package hashtable;
+
+/**
+ * @ Author: Xuelong Liao
+ * @ Description:
+ * @ Date: created in 15:49 2018/5/1
+ * @ ModifiedBy:
+ */
+public class Point {
+    int x;
+    int y;
+    Point() { x = 0; y = 0; }
+    Point(int a, int b) { x = a; y = b; }
+}
