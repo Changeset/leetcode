78685510ac3e1a31b382a69a86fcf702e19be549
Changeset <695700827@qq.com>
2018-05-11 16:46:27 +0800

    update singe number III cpp code

--- Cpp/single_number_III.cpp
+++ Cpp/single_number_III.cpp
@@ -2,7 +2,8 @@ class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
         vector<int> res = {0, 0};
-        int diff = 0;
+        int diff = accumulate(nums.begin(), nums.end(), 0, bit_xor<int>());
+        diff &= -diff;
         for (int num : nums) {
             if ((num & diff) == 0)
                 res[0] ^= num;
