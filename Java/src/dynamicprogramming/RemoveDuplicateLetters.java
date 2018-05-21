14aa5ab7545108ec14d93c1db80761f8f1e004cc
Changeset <695700827@qq.com>
2018-05-21 20:15:43 +0800

    update solution to dynamic programming problems

--- Java/src/dynamicprogramming/RemoveDuplicateLetters.java
+++ Java/src/dynamicprogramming/RemoveDuplicateLetters.java
@@ -1,5 +1,8 @@
 package dynamicprogramming;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @ Author: Xuelong Liao
  * @ Description:
@@ -21,5 +24,44 @@ public class RemoveDuplicateLetters {
     public static void main(String[] args) {
         RemoveDuplicateLetters r = new RemoveDuplicateLetters();
         System.out.println(r.removeDuplicateLetters("bcsdabcd"));
+    }
+
+    public String removeDupLetters(String s) {
+        if (s == null || s.length() <= 1) return s;
+
+        Map<Character, Integer> lastPosMap = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            lastPosMap.put(s.charAt(i), i);
+        }
+
+        char[] result = new char[lastPosMap.size()];
+        int begin = 0, end = findMinLastPos(lastPosMap);
+
+        for (int i = 0; i < result.length; i++) {
+            char minChar = 'z' + 1;
+            for (int k = begin; k <= end; k++) {
+                if (lastPosMap.containsKey(s.charAt(k)) && s.charAt(k) < minChar) {
+                    minChar = s.charAt(k);
+                    begin = k+1;
+                }
+            }
+
+            result[i] = minChar;
+            if (i == result.length-1) break;
+
+            lastPosMap.remove(minChar);
+            if (s.charAt(end) == minChar) end = findMinLastPos(lastPosMap);
+        }
+
+        return new String(result);
+    }
+
+    private int findMinLastPos(Map<Character, Integer> lastPosMap) {
+        if (lastPosMap == null || lastPosMap.isEmpty()) return -1;
+        int minLastPos = Integer.MAX_VALUE;
+        for (int lastPos : lastPosMap.values()) {
+            minLastPos = Math.min(minLastPos, lastPos);
+        }
+        return minLastPos;
     }
 }
