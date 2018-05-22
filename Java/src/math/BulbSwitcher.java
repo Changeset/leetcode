d6ce07a6ada63d2fe22fa7c189b4df8c52e18e77
Changeset <695700827@qq.com>
2018-05-22 17:17:07 +0800

    update solution to math problems

---
+++ Java/src/math/BulbSwitcher.java
@@ -0,0 +1,13 @@
+package math;
+
+/**
+ * @ Author: Xuelong Liao
+ * @ Description:
+ * @ Date: created in 14:48 2018/5/22
+ * @ ModifiedBy:
+ */
+public class BulbSwitcher {
+    public int bulbSwitch(int n) {
+        return (int)Math.sqrt(n);
+    }
+}
