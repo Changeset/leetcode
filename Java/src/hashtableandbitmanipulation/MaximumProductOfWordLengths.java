1e3f773d2f678fb40cc0624241d6fd4bfd2ba0b9
Changeset <695700827@qq.com>
2018-05-22 17:16:48 +0800

    update solution to bit manipulation problems

---
+++ Java/src/hashtableandbitmanipulation/MaximumProductOfWordLengths.java
@@ -0,0 +1,36 @@
+package hashtableandbitmanipulation;
+
+/**
+ * @ Author: Xuelong Liao
+ * @ Description:
+ * @ Date: created in 9:04 2018/5/22
+ * @ ModifiedBy:
+ */
+public class MaximumProductOfWordLengths {
+    public int maxProduct(String[] words) {
+        if (words == null || words.length == 0) return 0;
+        int len = words.length;
+        int[] value = new int[len];
+        for (int i = 0; i < len; i++) {
+            String tmp = words[i];
+            value[i] = 0;
+            for (int j = 0; j < tmp.length(); j++) {
+                value[i] |= 1 << (tmp.charAt(j) - 'a');
+            }
+        }
+        int maxProduct = 0;
+        for (int i = 0; i < len; i++) {
+            for (int j = i + 1; j < len; j++) {
+                if ((value[i] & value[j]) == 0 && (words[i].length() * words[j].length() > maxProduct))
+                    maxProduct = words[i].length() * words[j].length();
+            }
+        }
+        return maxProduct;
+    }
+
+    public static void main(String[] args) {
+        MaximumProductOfWordLengths m = new MaximumProductOfWordLengths();
+        String[] words = {"abcw","baz","foo","bar","xtfn","abcdef"};
+        System.out.println(m.maxProduct(words));
+    }
+}
