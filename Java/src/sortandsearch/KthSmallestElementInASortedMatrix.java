fc70d995c567fd2772563a5ea0c8b639034bf1b2
Changeset <695700827@qq.com>
2018-05-31 20:31:05 +0800

    update solution to sort and search problems

--- Java/src/sortandsearch/KthSmallestElementInASortedMatrix.java
+++ Java/src/sortandsearch/KthSmallestElementInASortedMatrix.java
@@ -34,4 +34,28 @@ public class KthSmallestElementInASortedMatrix {
         }
         return pq.poll().val;
     }
+    public int kthSmallest1(int[][] matrix, int k) {
+        int lo = matrix[0][0], hi = matrix[matrix.length - 1][matrix[0].length - 1] + 1;
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            int count = 0, j = matrix[0].length - 1;
+            for (int i = 0; i < matrix.length; i++) {
+                while (j >= 0 && matrix[i][j] > mid) j--;
+                count += (j + 1);
+            }
+            if (count < k) lo = mid + 1;
+            else hi = mid;
+        }
+        return lo;
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {1, 5, 9},
+                {10, 11, 13},
+                {12, 13, 15}
+        };
+        KthSmallestElementInASortedMatrix k = new KthSmallestElementInASortedMatrix();
+        System.out.println(k.kthSmallest1(matrix, 8));
+    }
 }
