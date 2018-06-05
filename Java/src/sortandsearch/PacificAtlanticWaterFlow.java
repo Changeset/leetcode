411bb6c3b2b407d938a10f01e445032c5ad8de24
Changeset <695700827@qq.com>
2018-06-05 21:30:00 +0800

    update solution to sort and search problems

---
+++ Java/src/sortandsearch/PacificAtlanticWaterFlow.java
@@ -0,0 +1,57 @@
+package sortandsearch;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @ Author: Xuelong Liao
+ * @ Description:
+ * @ Date: created in 15:21 2018/6/5
+ * @ ModifiedBy:
+ */
+public class PacificAtlanticWaterFlow {
+    public List<int[]> pacificAtlantic(int[][] matrix) {
+        List<int[]> res = new LinkedList<>();
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return res;
+        }
+        int n = matrix.length, m = matrix[0].length;
+        boolean[][] pacific = new boolean[n][m];
+        boolean[][] atlantic = new boolean[n][m];
+        for (int i = 0; i < n; i++) {
+            dfs(matrix, pacific, Integer.MIN_VALUE, i, 0);
+            dfs(matrix, atlantic, Integer.MIN_VALUE, i, m - 1);
+        }
+        for (int i = 0; i < m; i++) {
+            dfs(matrix, pacific, Integer.MIN_VALUE, 0, i);
+            dfs(matrix, atlantic, Integer.MIN_VALUE, n - 1, i);
+        }
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+                if (pacific[i][j] && atlantic[i][j])
+                    res.add(new int[] {i, j});
+        return res;
+    }
+
+    private void dfs(int[][] matrix, boolean[][] visited, int height, int x, int y) {
+        int n = matrix.length, m = matrix[0].length;
+        if (x < 0 || x >= n || y < 0 || y >= m || visited[x][y] || matrix[x][y] < height)
+            return;
+        visited[x][y] = true;
+        for (int[] d : matrix) {
+            dfs(matrix, visited, matrix[x][y], x + d[0], y + d[1]);
+        }
+    }
+
+    public static void main(String[] args) {
+        PacificAtlanticWaterFlow p = new PacificAtlanticWaterFlow();
+        int[][] matrix = {
+                {1,2,2,3,5},
+                {3,2,3,4,4},
+                {2,4,5,3,1},
+                {6,7,1,4,5},
+                {5,1,1,2,4}
+        };
+        System.out.println(p.pacificAtlantic(matrix));
+    }
+}
