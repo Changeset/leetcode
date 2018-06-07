789f94e8516b617ca1853bb4d0cf1f03391cdc65
Changeset <695700827@qq.com>
2018-06-07 21:32:38 +0800

    update solution to Linked List problems

---
+++ Java/src/linkedList/AddTwoNumbersII.java
@@ -0,0 +1,38 @@
+package linkedList;
+
+import java.util.Stack;
+
+/**
+ * @ Author: Xuelong Liao
+ * @ Description:
+ * @ Date: created in 14:36 2018/6/7
+ * @ ModifiedBy:
+ */
+public class AddTwoNumbersII {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        Stack<Integer> s1 = new Stack<Integer>();
+        Stack<Integer> s2 = new Stack<Integer>();
+
+        while (l1 != null) {
+            s1.push(l1.val);
+            l1 = l1.next;
+        }
+        while (l2 != null) {
+            s2.push(l2.val);
+            l2 = l2.next;
+        }
+
+        int sum = 0;
+        ListNode list = new ListNode(0);
+        while (!s1.empty() || !s2.empty()) {
+            if (!s1.empty()) sum += s1.pop();
+            if (!s2.empty()) sum += s2.pop();
+            list.val = sum % 10;
+            ListNode head = new ListNode(sum / 10);
+            head.next = list;
+            list = head;
+            sum /= 10;
+        }
+        return list.val == 0 ? list.next : list;
+    }
+}
