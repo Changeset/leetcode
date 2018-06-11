cb8e1e50a699ad14db14cfad45f4d8e4ac466799
Changeset <695700827@qq.com>
2018-06-11 21:31:42 +0800

    update solution to dynamic programming problems

---
+++ Java/src/dynamicprogramming/ConcatenatedWords.java
@@ -0,0 +1,40 @@
+package dynamicprogramming;
+
+import java.util.*;
+
+/**
+ * @ Author: Xuelong Liao
+ * @ Description:
+ * @ Date: created in 19:42 2018/6/11
+ * @ ModifiedBy:
+ */
+public class ConcatenatedWords {
+    public List<String> findAllConcatenatedWordsInADict(String[] words) {
+        List<String> result = new ArrayList<>();
+        Set<String> preWords = new HashSet<>();
+        Arrays.sort(words, (s1, s2) -> Integer.compare(s1.length(), s2.length()));
+        for (int i = 0; i < words.length; i++) {
+            if (canForm(words[i], preWords)) {
+                result.add(words[i]);
+            }
+            preWords.add(words[i]);
+        }
+        return result;
+    }
+
+    private boolean canForm(String word, Set<String> dict) {
+        if (dict.isEmpty()) return false;
+        boolean[] dp = new boolean[word.length() + 1];
+        dp[0] = true;
+        for (int i = 1; i <= word.length(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (!dp[j]) continue;
+                if (dict.contains(word.substring(j, i))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[word.length()];
+    }
+}
